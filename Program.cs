a490c2c3cff9325ed27ad366ce34464e75e02efb
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-17 12:02:14 +0530

    [Madhur]-adding addition part

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20, result;
+
+            //Addition
+            result = a + b;
+            Console.WriteLine("Result is : " + result);
+        }
+    }
+}
